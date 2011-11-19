493dbe076e2e73a0bf0d1724b57d14e738c84b47
Artem Baguinski <artm@v2.nl>
2011-11-19 09:00:22 +0100

    public methods upfront
    also made private some methods of no concern to the user

--- Osc.cs
+++ Osc.cs
@@ -5,37 +5,6 @@ using System.Net;
 
 public class Osc
 {
-	public static String ReadString(byte[] data, ref int offset)
-	{
-		int count = 0;
-		for(; (offset+count)<data.Length && data[offset+count]!=0;count++);
-		String result = Encoding.UTF8.GetString(data, offset, count);
-		offset += PaddedLength(count);
-		return result;
-	}
-
-	public static int ReadInt32(byte[] data, ref int offset)
-	{
-		int res = IPAddress.NetworkToHostOrder( BitConverter.ToInt32(data, offset) );
-		offset += 4;
-		return res;
-	}
-
-	public static float ReadFloat32(byte[] data, ref int offset)
-	{
-		float res;
-		if (!BitConverter.IsLittleEndian)
-			res = BitConverter.ToSingle(data, offset);
-		else {
-			byte[] sub = new byte[4];
-			Array.Copy(data, offset, sub, 0, 4);
-			Array.Reverse(sub);
-			res = BitConverter.ToSingle(sub, 0);
-		}
-		offset += 4;
-		return res;
-	}
-
 	public static IEnumerable ParseArgs(byte[] data, int offset, string types)
 	{
 		foreach(char t in types.Substring(1)) {
@@ -62,6 +31,60 @@ public class Osc
 		foreach(object o in ParseArgs(data,offset,types)) {
 			res[i++] = o;
 		}
+		return res;
+	}
+
+	public static byte[] FromArray(params object[] args)
+	{
+		StringBuilder types = new StringBuilder(args.Length);
+		int size = 0;
+
+		foreach(object o in args) {
+			types.Append(OscTypeOf(o));
+			size += OscSizeOf(o);
+		}
+		types[0] = ',';
+		size += OscSizeOf(types.Length);
+
+		byte[] data = new byte[size];
+		int offset = 0;
+		Put(args[0] as String, data, ref offset);
+		Put(types.ToString(), data, ref offset);
+		for(int i = 1; i<args.Length; i++) {
+			Put(args[i], data, ref offset);
+		}
+		return data;
+	}
+
+	#region details
+	static String ReadString(byte[] data, ref int offset)
+	{
+		int count = 0;
+		for(; (offset+count)<data.Length && data[offset+count]!=0;count++);
+		String result = Encoding.UTF8.GetString(data, offset, count);
+		offset += PaddedLength(count);
+		return result;
+	}
+
+	static int ReadInt32(byte[] data, ref int offset)
+	{
+		int res = IPAddress.NetworkToHostOrder( BitConverter.ToInt32(data, offset) );
+		offset += 4;
+		return res;
+	}
+
+	static float ReadFloat32(byte[] data, ref int offset)
+	{
+		float res;
+		if (!BitConverter.IsLittleEndian)
+			res = BitConverter.ToSingle(data, offset);
+		else {
+			byte[] sub = new byte[4];
+			Array.Copy(data, offset, sub, 0, 4);
+			Array.Reverse(sub);
+			res = BitConverter.ToSingle(sub, 0);
+		}
+		offset += 4;
 		return res;
 	}
 
@@ -130,27 +153,6 @@ public class Osc
 		Encoding.ASCII.GetBytes(s,0,s.Length,data,offset);
 		offset += PaddedLength(s);
 	}
-
-	public static byte[] FromArray(params object[] args)
-	{
-		StringBuilder types = new StringBuilder(args.Length);
-		int size = 0;
-
-		foreach(object o in args) {
-			types.Append(OscTypeOf(o));
-			size += OscSizeOf(o);
-		}
-		types[0] = ',';
-		size += OscSizeOf(types.Length);
-
-		byte[] data = new byte[size];
-		int offset = 0;
-		Put(args[0] as String, data, ref offset);
-		Put(types.ToString(), data, ref offset);
-		for(int i = 1; i<args.Length; i++) {
-			Put(args[i], data, ref offset);
-		}
-		return data;
-	}
+	#endregion
 }
 
